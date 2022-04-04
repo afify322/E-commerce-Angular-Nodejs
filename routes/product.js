d33cbd500a7c05f9704e63ca8a404844b0219feb
afify322 <76813264+afify322@users.noreply.github.com>
2022-04-04 17:37:14 +0200

    Update product.js

--- routes/product.js
+++ routes/product.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express.Router();
 const Product = require('../controllers/product');
 const {productValidation} = require('../middlewares/productValidation');
-
+const {parser} = require('../helpers/imageUpload')
 
 router.get('/', Product.findAllProduct);
 router.get('/:id',Product.findProductById);
 router.get('/category/:id',Product.findProductByCategory);
-router.post('/', productValidation, Product.addProduct);
+router.post('/',parser, Product.addProduct);
 router.patch('/:id', Product.EditProductById);
 router.delete('/:id', Product.deleteProduct);
 
