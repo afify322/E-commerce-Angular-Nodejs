35518dbc08c9db98ab97c05aadb9e4e9d5eb3d15
afify322 <76813264+afify322@users.noreply.github.com>
2022-04-23 05:01:01 +0200

    Update db.js

--- helpers/db.js
+++ helpers/db.js
@@ -3,7 +3,7 @@ const mongoose=require('mongoose');
 exports.connect= (async()=> {
     try {
         
-    var client= await mongoose.connect("mongodb+srv://ITI:ITI@ecommerce.aqzfd.mongodb.net/myFirstDatabase?authSource=admin&replicaSet=atlas-xdveji-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true");
+    var client= await mongoose.connect(process.env.mongo);
     
     } catch (error) {
      console.log(error);
